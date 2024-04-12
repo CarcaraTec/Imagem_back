032edd78acc6a1368d351aa228aec7aeddadd398
alantrs <alanaraujotrs@gmail.com>
2024-04-12 00:15:15 -0300

    IM-29 mudanca nome das variaveis e ajustes no codigo

--- app/service/mapaService.py
+++ app/service/mapaService.py
@@ -1,44 +1,40 @@
 import folium
 from folium.plugins import HeatMap
 from app.repository.dadosCollection_repository import DadosCollectionRepository
+import math
 
 class MapaService:
 
     def __init__(self):
         self.repository = DadosCollectionRepository()
 
-    def gerar_mapa_de_calor(self, sentimento: int = None) -> str:
-        m = folium.Map([47.3, 8.5], zoom_start=4)
+    def gerar_mapa_de_calor(self, sentimento: str = None) -> str:
+        m = folium.Map([47.3, 8.5], zoom_start=5)
 
-        filter = {'latitude': {'$exists': True}, 'longitude': {'$exists': True}}
+        filter = {'lat': {'$exists': True}, 'lng': {'$exists': True}}
 
         if sentimento is not None:
-            filter['sentiment'] = int(sentimento)
+            filter['sentiment'] = sentimento
 
         data = self.repository.select_many(filter)
 
-        coordinates = []
-
-        for elem in data:
-            lat = elem['latitude']
-            lon = elem['longitude']
-            coordinates.append([lat, lon])
+        coordinates = [[elem.get('lat'), elem.get('lng')] for elem in data if not math.isnan(elem.get('lat')) and not math.isnan(elem.get('lng'))]
 
         HeatMap(coordinates).add_to(m)
 
         return m.get_root().render()
 
     def gerar_mapa_marcador(self):
-        m = folium.Map([47.3, 8.5], zoom_start=4)
+        m = folium.Map([47.3, 8.5], zoom_start=5)
 
-        filter = {'latitude': {'$exists': True}, 'longitude': {'$exists': True}}
+        filter = {'lat': {'$exists': True}, 'lng': {'$exists': True}}
 
         data = self.repository.select_many(filter)
 
-        coordinates = [{'location': [elem['latitude'], elem['longitude']], 'sentiment': elem.get('sentiment', 2)} for elem in data]
+        coordinates = [{'location': [elem.get('lat'), elem.get('lng')],'sentiment': elem.get('sentiment')}for elem in data if not (math.isnan(elem.get('lat')) or math.isnan(elem.get('lng')))]
 
         def get_sentiment_color(sentiment):
-            sentiment_colors = {1: 'DarkGreen', 0: 'DarkRed', 2: 'blue'}
+            sentiment_colors = {'Positive': 'DarkGreen', 'Negative': 'DarkRed', 'Neutral': 'blue'}
             return sentiment_colors.get(sentiment, 'gray')
 
         for coord in coordinates:
