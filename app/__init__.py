12467171889d0c251f2aa821cdf663b49033e34e
alantrs <alanaraujotrs@gmail.com>
2024-04-12 21:33:46 -0300

    IM-30 adicionado counts de registros

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.controller.mapaController import mapa_bp
+from app.controller.CalculoController import calculo_bp
 from app.mongoConfig.connection_options.connection import DBconnectionHandler
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ db_handler = DBconnectionHandler()
 db_handler.connect_to_db()
 
 app.register_blueprint(mapa_bp, url_prefix='/mapa')
+app.register_blueprint(calculo_bp, url_prefix='/calculos')
 
 app.config['db_handler'] = db_handler
---
+++ app/controller/CalculoController.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, request, jsonify
+from app.service.calculoService import CalculoService
+
+calculo_bp = Blueprint('calculo', __name__)
+calculo_service = CalculoService()
+
+@calculo_bp.route("/cards")
+def teste():
+    return calculo_service.gerar_calculos_cards()
