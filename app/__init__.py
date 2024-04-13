7cedde80c9d7d689b5dcacc2e95167bb9fe55889
alantrs <alanaraujotrs@gmail.com>
2024-04-13 20:01:48 -0300

    Configuração de CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from app.controller.mapaController import mapa_bp
 from app.controller.CalculoController import calculo_bp
 from app.mongoConfig.connection_options.connection import DBconnectionHandler
 
 app = Flask(__name__)
+CORS(app)
 
 db_handler = DBconnectionHandler()
 db_handler.connect_to_db()
