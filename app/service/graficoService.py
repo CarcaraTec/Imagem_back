69313ad8bd536b491db2fd26432733a35f973f74
alantrs <alanaraujotrs@gmail.com>
2024-04-13 22:32:18 -0300

    IM-33 nome do mes

--- app/service/graficoService.py
+++ app/service/graficoService.py
@@ -8,6 +8,14 @@ class GraficoService:
     def __init__(self):
         self.repository = DadosCollectionRepository()
 
+
+    def traduzir_data_para_mes(self, data_str):
+        data_obj = datetime.strptime(data_str, '%m/%d/%Y')
+
+        nome_mes = data_obj.strftime('%B') 
+
+        return nome_mes
+    
     def gerar_grafico_sentimentos(self):
         filtro = {}
         dados = self.repository.select_random(filtro, 100)
@@ -21,6 +29,14 @@ class GraficoService:
         for dado in dados:
             sentiment = dado.get('sentiment')
             if sentiment in sentiment_data:
-                sentiment_data[sentiment].append(dado['Review_Date'])  # Adiciona apenas 'data' à lista correspondente
+                data_str = dado.get('Review_Date')
+                
+                if data_str:
+                    nome_mes = self.traduzir_data_para_mes(data_str)
+                    
+                    sentiment_data[sentiment].append(nome_mes)
 
         return sentiment_data
+        
+
+    
