2a3915d0fe7a4d9b594b6260764efbc6b1de1d2b
alantrs <alanaraujotrs@gmail.com>
2024-04-26 15:10:51 -0300

    IM-48 top 5 hoteis mais bem avaliados

--- app/service/calculoService.py
+++ app/service/calculoService.py
@@ -1,9 +1,9 @@
-from app.repository.dadosCollection_repository import DadosCollectionRepository
+from app.repository.reviews_analyzed_repository import ReviewsAnalyzedRepository
 
 class CalculoService:
 
     def __init__(self):
-        self.repository = DadosCollectionRepository()
+        self.repository = ReviewsAnalyzedRepository()
 
     def gerar_calculos_cards(self):
         return self.repository.count_sentiments()
