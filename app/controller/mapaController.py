24f182a1e45c5b37ef22dff8a09bffe27a45afca
alantrs <alanaraujotrs@gmail.com>
2024-05-20 21:25:01 -0300

    IM-102 filtro nos mapas

--- app/controller/mapaController.py
+++ app/controller/mapaController.py
@@ -7,10 +7,16 @@ mapa_service = MapaService()
 @mapa_bp.route("/calor")
 def mapa_de_calor():
     sentimento = request.args.get('sentiment') 
-    mapa_renderizado = mapa_service.gerar_mapa_de_calor(sentimento)
+    cidade = request.args.get('cidade') 
+    data_inicio = request.args.get('data_inicio')
+    data_fim = request.args.get('data_fim')
+    mapa_renderizado = mapa_service.gerar_mapa_de_calor(sentimento, cidade, data_inicio, data_fim)
     return mapa_renderizado
 
 @mapa_bp.route("/marcador")
 def mapa_com_marcador():
-    mapa_renderizado = mapa_service.gerar_mapa_marcador()
+    cidade = request.args.get('cidade') 
+    data_inicio = request.args.get('data_inicio')
+    data_fim = request.args.get('data_fim')
+    mapa_renderizado = mapa_service.gerar_mapa_marcador(cidade, data_inicio, data_fim)
     return mapa_renderizado
